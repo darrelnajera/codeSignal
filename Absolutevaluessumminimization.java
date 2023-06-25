49f79f065f26922c4d3b8ddc0c5d71a81ced1ffc
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-06-25 12:05:09 -0500

    Update Absolutevaluessumminimization.java

--- Absolutevaluessumminimization.java
+++ Absolutevaluessumminimization.java
@@ -2,7 +2,7 @@ public class Absolutevaluessumminimization {
     int solution(int[] a) {
         //in this array of ints, the number closest to the rest is the one in the middle. 
         
-        // we take the last number in the array and we subtract by 1 and divide by 2
+        // we take the length of the array and we subtract by 1 and divide by 2
         return a[(a.length - 1) / 2 ];
         }
         
