9c938c12687d81e675ae0b3b26e4f8192706a0bc
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-06-26 16:04:18 -0500

    Update Ismacfortyeightaddress.java

--- Ismacfortyeightaddress.java
+++ Ismacfortyeightaddress.java
@@ -1,5 +1,6 @@
 public class Ismacfortyeightaddress {
     boolean solution(String inputString) {
+        //use regex to return from numbers 0-9 and letters a-f separated by {5 hyphens}
         return inputString.matches("^([0-9A-F][0-9A-F]-){5}[0-9A-F][0-9A-F]$");
     
     }
