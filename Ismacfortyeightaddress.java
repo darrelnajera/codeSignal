de6a36e09cc550268f5fce8487cf15957ee4a786
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-06-26 16:03:03 -0500

    Add files via upload

---
+++ Ismacfortyeightaddress.java
@@ -0,0 +1,8 @@
+public class Ismacfortyeightaddress {
+    boolean solution(String inputString) {
+        return inputString.matches("^([0-9A-F][0-9A-F]-){5}[0-9A-F][0-9A-F]$");
+    
+    }
+    
+    
+}
