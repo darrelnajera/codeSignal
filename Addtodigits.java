ed1c5522cdbf4b83a990c84ae2bd05156e2a3bf7
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-08-09 20:53:33 -0500

    Add files via upload

---
+++ Addtodigits.java
@@ -0,0 +1,7 @@
+public class Addtodigits {
+    int solution(int n) {
+        //get modulus of number and divide that number by 10 and add two numbers together
+        return (n%10 + n/10);
+    }
+    
+}
