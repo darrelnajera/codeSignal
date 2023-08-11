19f6f0d4f751c990dead45739faa68ebaf1fa1d0
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-08-11 00:21:06 -0500

    Add files via upload

---
+++ MaxMultiple.java
@@ -0,0 +1,7 @@
+public class MaxMultiple {
+    int solution(int divisor, int bound) {
+        //divide bound by divisor. multiply that by divisor to get quotient with no remainder
+    return (bound / divisor) * divisor;
+    }
+    
+}
