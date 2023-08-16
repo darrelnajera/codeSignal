9efec3a863e2887f035e2665b56cf1e6ea13f2f6
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-08-15 23:38:17 -0500

    Add files via upload

---
+++ Reachnextlevel.java
@@ -0,0 +1,8 @@
+public class Reachnextlevel {
+    boolean solution(int experience, int threshold, int reward) {
+
+        //simply add both experience and return to see if more than threshold
+        return experience+reward>=threshold;
+    }
+    
+}
