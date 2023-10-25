c08702b94a6dbf554129a3a8a2fd4223bbe4ed58
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-10-24 23:52:08 -0500

    Update Equalpairofbits.java

--- Equalpairofbits.java
+++ Equalpairofbits.java
@@ -1,5 +1,7 @@
 public class Equalpairofbits {
     int solution(int n, int m) {
+        //perfom logical negationsin each bit
+        //return n and the position of the found pair
         return Integer.lowestOneBit(~(n^m));
       }
 }
