8dfd30c7ed86723c1f8cb833253dde56ff420ee8
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-10-24 23:44:43 -0500

    Update Differentrightmostbit.java

--- Differentrightmostbit.java
+++ Differentrightmostbit.java
@@ -1,5 +1,7 @@
 public class Differentrightmostbit {
+    //int m and m in class
 int solution(int n, int m) {
+    //return the bit in lowest order from n to the power of m
     return Integer.lowestOneBit(n^m) ;
   }
 }
