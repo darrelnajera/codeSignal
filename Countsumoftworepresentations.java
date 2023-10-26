7e46181844b2582d5d0a7a1cec61106176173c58
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-10-25 23:36:38 -0500

    Add files via upload

---
+++ Countsumoftworepresentations.java
@@ -0,0 +1,15 @@
+public class Countsumoftworepresentations {
+    
+int solution(int n, int l, int r) {
+
+    int result = 0;
+
+   for (int a = l; a <= r; a++) {
+       int b = n - a;
+       if (b >= l && b <= r && b >= a) {
+           result++;
+       }
+   }
+   return result;
+}
+}
