9e7d2471255f6234b754349869e12c9181b155f6
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-10-25 23:43:33 -0500

    Update Willyou.java

--- Willyou.java
+++ Willyou.java
@@ -1,7 +1,9 @@
 public class Willyou {
     boolean solution(boolean young, boolean beautiful, boolean loved) {
+        //if boolean doesnt equal loved return other two
         if (!loved) {
             return (young && beautiful);
+            //else dont return other two
         } else {
             return !(young && beautiful);
         }
