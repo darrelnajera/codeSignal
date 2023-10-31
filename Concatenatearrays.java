43f6492f9f70a17e4ea53ce8e4201f9259c2a416
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-10-30 23:47:26 -0500

    Add files via upload

---
+++ Concatenatearrays.java
@@ -0,0 +1,17 @@
+public class Concatenatearrays {
+    int[] solution(int[] a, int[] b) {
+        int[] ans = new int[a.length + b.length];
+        int j = 0;
+        
+        for (int i=0;i<a.length;i++) {
+            ans[j++] = a[i];
+        }
+        
+        for (int i=0;i<b.length;i++) {
+            ans[j++] = b[i];
+        }
+        
+        return ans;
+    }
+    
+}
