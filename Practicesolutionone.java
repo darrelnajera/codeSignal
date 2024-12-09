ed8bb85bf1157ae94eaca83fa8699f399540a3d1
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-08 23:50:49 -0600

    Add files via upload

---
+++ Practicesolutionone.java
@@ -0,0 +1,18 @@
+public class Practicesolutionone {
+    // TODO: Declare a class named 'Solution' (other class names won't work)
+// Make sure NOT to make your class 'public'
+
+    // TODO: Inside your class, define the main 'public static void main' method that Java looks for
+
+        // TODO: Use a print statement to display a message "3, 2, 1... Lift off!" to the console
+        
+class Solution{
+    //Implement main method
+    class Solution{
+        public static void main(String[] args) {
+            System.out.println("3, 2, 1... Lift off!");
+            
+        }
+    }
+}
+}
