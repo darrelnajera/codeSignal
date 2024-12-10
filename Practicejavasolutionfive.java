cfe97994a63d9c047940f8688d5f17957d7f37ed
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-09 23:53:11 -0600

    Add files via upload

---
+++ Practicejavasolutionfive.java
@@ -0,0 +1,17 @@
+public class Practicejavasolutionfive {
+    class Solution {
+        public static void main(String[] args) {
+            // TODO: Declare a constant for Earth's orbital speed and give it a value of 108000 (km/h)       
+            
+            // TODO: Declare a variable for the number of planets in the Solar System and assign it to 8
+            
+            // TODO: Print out both the orbital speed of the Earth and the number of planets using print statements
+            //correct syntax for constant and int
+            final int EARTHS_ORBITAL_SPEEDKMH = 108000;
+            int NumberofPlanets = 8;
+            //print out line with correct spacing
+            System.out.println("Earth's orbital speed: " + EARTHS_ORBITAL_SPEEDKMH + " km/h" );
+            System.out.println("Number of planets: " + NumberofPlanets);
+        }
+    }
+}
