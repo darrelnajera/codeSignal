484ac4616477336d4134a8aa903af636f075a11e
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-09 23:52:34 -0600

    Add files via upload

---
+++ Practicejavasolutionthree.java
@@ -0,0 +1,11 @@
+public class Practicejavasolutionthree {
+    class Solution {
+        public static void main(String[] args) {
+            int earthOrbitRadius = 149600000; // in kilometers
+            System.out.println("The Earth's orbit radius is " + earthOrbitRadius + " kilometers.");
+            //Fix bug which is to make int and constant and fix the syntax that follows it.
+            final int EIGHT_PLANETS = 8;
+            System.out.println("There are " + EIGHT_PLANETS + " planets in the Solar System.");
+        }
+    }
+}
