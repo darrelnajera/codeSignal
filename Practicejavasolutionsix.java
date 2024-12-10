c15e08093566e33189610561466d652a53a559d5
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-09 23:59:26 -0600

    Add files via upload

---
+++ Practicejavasolutionsix.java
@@ -0,0 +1,10 @@
+public class Practicejavasolutionsix {
+    class Solution {
+        public static void main(String[] args) {
+            // Define a variable with 'int' type representing days until the space launch
+            double countdownStart = 10.5;
+    
+            System.out.println("There are " + countdownStart + " days until the launch.");
+        }
+    }
+}
