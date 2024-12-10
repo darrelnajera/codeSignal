a95065f60f6f93613e3392e7c64587004cc36a91
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-09 23:52:50 -0600

    Add files via upload

---
+++ Practicejavasolutionfour.java
@@ -0,0 +1,14 @@
+public class Practicejavasolutionfour {
+    class Solution {
+        public static void main(String[] args) {
+            // TODO: Declare a constant for Earth's orbital speed and set its value to 29 km/s
+            int planetsInSolarSystem = 8; // Number of planets in the solar system
+            // TODO: Print out the string "Earth's orbital speed: <speed value> km/s
+            System.out.println("Number of planets: " + planetsInSolarSystem);
+            //declare constant with correct syntax
+            final int EARTH_ORBITAL_SPEEDKMS = 29;
+            //print out message with correct spacing
+            System.out.println("Earth's orbital speed: " + EARTH_ORBITAL_SPEEDKMS + " km/s");
+        }
+    }
+}
