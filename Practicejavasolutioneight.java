f427598368c5c77204ed66944100b3870575dbfe
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-10 23:42:40 -0600

    Add files via upload

---
+++ Practicejavasolutioneight.java
@@ -0,0 +1,25 @@
+public class Practicejavasolutioneight {
+    class Solution {
+        public static void main(String[] args) {
+            // TODO: Declare a float variable to store the distance from Earth to the Sun in kilometers         - 149600000.0 kilometers
+            // TODO: Declare an int variable to store the number of days Earth takes to orbit the Sun - 365 days
+            
+            // TODO: Print the distance of Earth from the Sun followed by " km from the Sun."
+            
+            // TODO: Print the number of days Earth takes to orbit the Sun, followed by " days to orbit the Sun."
+            
+            // TODO: Declare a boolean variable to store the truth about whether space is dark - it is!
+            
+            // TODO: Use a char variable to represent the first letter of 'Cosmos' - C
+            //declare float and int variable to print out in statements
+            float distanceFromEarthToSunKm = 149600000;
+            int numberOfDays = 365;
+            // print out with correct spacing
+            System.out.println("Earth is " + distanceFromEarthToSunKm + " km from the Sun.");
+            System.out.println("Earth takes" + numberOfDays + " days to orbit the Sun.");
+            //declare boolean and char variable
+            boolean isSpaceDark = true;
+            char cosmos = 'C';
+        }
+    }
+}
