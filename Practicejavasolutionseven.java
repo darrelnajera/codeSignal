463191787cbf0a0da83e5e8c9b93ca63b851f5eb
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-10 23:42:23 -0600

    Add files via upload

---
+++ Practicejavasolutionseven.java
@@ -0,0 +1,13 @@
+public class Practicejavasolutionseven {
+    class Solution {
+        public static void main(String[] args) {
+            int numOfPlanets = 8;
+            //change error which is float to double
+            double rocketWeight = 1500.5;
+            String message = "Ready for space exploration!";
+            System.out.println("There are " + numOfPlanets + " known planets.");
+            System.out.println("Our rocket weighs " + rocketWeight + " tons.");
+            System.out.println(message);
+        }
+    }
+}
