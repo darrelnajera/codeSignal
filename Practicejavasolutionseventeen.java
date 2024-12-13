3d6589025fa315c65482517ad502e8274b003258
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-12 23:39:41 -0600

    Add files via upload

---
+++ Practicejavasolutionseventeen.java
@@ -0,0 +1,14 @@
+public class Practicejavasolutionseventeen {
+    class Solution {
+        public static void main(String[] args) {
+            // A star's temperature in Kelvin
+            double starTemperature = 15000.95;
+            
+            // TODO: Convert the temperature from a double to a String
+            String temperatureText = Double.toString(starTemperature);
+            
+            // Displaying the temperature as text
+            System.out.println("Mission control, the star's temperature is: " + temperatureText + "K");
+        }
+    }
+}
