646271a1a0961b939a4169370f992707cb2c456b
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-12 23:39:54 -0600

    Add files via upload

---
+++ Practicejavasolutioneighteen.java
@@ -0,0 +1,16 @@
+public class Practicejavasolutioneighteen {
+    class Solution {
+        public static void main(String[] args) {
+            int planetNumber = 7;
+            //assign int to double
+            double planetNumberDouble = planetNumber;
+            //use double to string method
+            String planetNumberStr = Double.toString(planetNumberDouble);
+            // TODO: Convert the planetNumber integer to a double using casting.
+            // TODO: Now, convert the new double to a String
+            
+            // Printing out the String value after conversion.
+            System.out.println("In our galaxy, the planet count is: " + planetNumberStr);
+        }
+    }
+}
