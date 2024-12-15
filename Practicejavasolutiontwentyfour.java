dc867726dcfd2ae0dd1667553d3c5881dcdb79bd
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-14 23:50:06 -0600

    Add files via upload

---
+++ Practicejavasolutiontwentyfour.java
@@ -0,0 +1,12 @@
+public class Practicejavasolutiontwentyfour {
+    class Solution {
+        public static void main(String[] args) {
+            //use variable we will compare
+            int cometSpeed = 100;
+            //use ternary operator two decide which action will happen from the condition
+            String action = (cometSpeed < 50) ? "Chasing comet!" : "Comet too fast! Let it go!";
+            //print out action
+            System.out.println(Action);
+        }
+    }
+}
