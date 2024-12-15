f42c844af27b851dc1fc02fef4e6e8b0f1129151
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-14 23:48:44 -0600

    Add files via upload

---
+++ Practicejavasolutiontwentyseven.java
@@ -0,0 +1,18 @@
+public class Practicejavasolutiontwentyseven {
+    class Solution {
+        public static void main(String[] args) {
+            String spaceExplorer = "Cosmo";
+            int discoveredPlanets = 7;
+            
+            // TODO: Use the '+' operator to concatenate spaceExplorer, discoveredPlanets and helper message strings to create a report.
+            String report1 = ; // Replace this with your concatenation code using '+'
+    
+            String excitement = " This is amazing!";
+            
+            // TODO: Complete the following line to concatenate the excitement message to the report using the 'concat' method.
+            String report2 = report1.concat(___);
+            
+            System.out.println(report2);
+        }
+    }
+}
