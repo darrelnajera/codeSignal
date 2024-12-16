00dc3eabc2116d523f32f335031f30094c393e1a
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-16 12:55:12 -0600

    Add files via upload

---
+++ Practicejavasolutiontwentyeight.java
@@ -0,0 +1,24 @@
+public class Practicejavasolutiontwentyeight {
+    public static void main(String[] args) {
+        
+        //First output
+        String socialMessage = "Hello, " + "Explorer! " + "Hope you're enjoying the Java Journey to the Stars course.";
+        
+        System.out.println(socialMessage);
+        
+        //second output
+        
+        //Use stringbuilder method
+        
+        StringBuilder socialMessage = new Stringbuilder();
+        //use append syntax to concatenate strings together
+        socialMessage.append("Hello, ").append("Explorer! ");
+        socialMessage.append("Hope you are enjoying the Stars to the journey course! ");
+        
+        //convert concatenated message to string
+        System.out.println(socialMessage)toString();
+        )
+    }
+    
+}
+}
